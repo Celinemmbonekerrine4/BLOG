0d00229d51a52738fe290759ff7cdb5859fa7e9d
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-21 15:22:16 +0300

    it will create the blog form and imported the wtf forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,12 @@
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField,TextAreaField,SubmitField
+
+
+class Blog(FlaskForm):
+    title = StringField('Write your blog title',validators=[Required()])
+    blog = TextAreaField('Type your best blog',validators=[required()])
     submit = SubmitField('Submit')
+
+
+
